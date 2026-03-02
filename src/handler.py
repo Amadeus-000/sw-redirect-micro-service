84d9bb5512ea4c535335dd8eb8e2bf73fa0909c6
Amadeus-000 <ma3hi68rada@hotmail.co.jp>
2026-03-02 17:21:11 +0900

    型ヒントを追加し、レスポンスメッセージを「Hello, World!」に変更

--- src/handler.py
+++ src/handler.py
@@ -1,12 +1,16 @@
 import json
 
+from aws_lambda_typing import context as context_
+from aws_lambda_typing.events import APIGatewayProxyEventV2
+from aws_lambda_typing.responses import APIGatewayProxyResponseV2
 
-def lambda_handler(event, context):
+
+def lambda_handler(event: APIGatewayProxyEventV2, context: context_.Context) -> APIGatewayProxyResponseV2:
     """Hello World Lambda function."""
     return {
         "statusCode": 200,
         "headers": {
             "Content-Type": "application/json",
         },
-        "body": json.dumps({"message": "Hello, World!!!!!!!!!!!!!!!!!!!!"}),
+        "body": json.dumps({"message": "Hello, World!"}),
     }
