725ac31b428c7b9d99e4635537d44f2f600419a7
Amadeus-000 <ma3hi68rada@hotmail.co.jp>
2026-03-02 19:48:17 +0900

    Refactor code structure for improved readability and maintainability

--- src/handler.py
+++ src/handler.py
@@ -1,8 +1,10 @@
+import os
 import json
-
+import requests
 from aws_lambda_typing import context as context_
 from aws_lambda_typing.events import APIGatewayProxyEventV2
 from aws_lambda_typing.responses import APIGatewayProxyResponseV2
+
 
 
 def lambda_handler(event: APIGatewayProxyEventV2, context: context_.Context) -> APIGatewayProxyResponseV2:
@@ -11,6 +13,12 @@ def lambda_handler(event: APIGatewayProxyEventV2, context: context_.Context) -> 
         return handle_redirect(event, context)
     if path == "redirect-test":
         return handle_redirect(event, context)
+    if path == "/test":
+        redirect_url=fetch_redirect_url("google")
+        return {
+            "statusCode": 200,
+            "body": json.dumps({"message": f"redirect_url: {redirect_url}"}),
+        }
     return {
             "statusCode": 302,
             "headers": {
@@ -61,3 +69,22 @@ def handle_redirect(event: APIGatewayProxyEventV2, context: context_.Context) ->
         },
         "body": f"Redirecting to {redirect_url}",
     }
+
+
+def fetch_redirect_url(work_id: str)-> str|None:
+    API_KEY = os.environ.get("MICRO_CMS_API_KEY")
+    if not API_KEY:
+        raise ValueError("MICRO_CMS_API_KEY environment variable is not set")
+    response = requests.get(
+        url ="https://sw-app.microcms.io/api/v1/redirect", 
+        headers={
+            "X-MICROCMS-API-KEY": API_KEY,
+        },
+            params={
+            "filters": f"work_id[equals]{work_id}",
+            "limit": 1,
+        }
+    )
+    response.raise_for_status()
+    data = response.json()
+    return data[0]["redirect_url"] if data else None
